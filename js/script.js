8ba89b24fa41cd5158dca24f35c03b54e2a8c2c5
Sogeking21 <vitorhugosleite@gmail.com>
2021-08-15 09:46:19 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+new SimpleSlide({
+  slide: 'principal',  // nome do atributo data-slide = "principal" 
+  nav: true,  // se deve ou não mostrar a navegação 
+  automática: true,  // se o slide deve passar automaticamente 
+  tempo: 5000,  // tempo de transição dos slides 
+  pauseOnHover: true,  // pausa a transição automática 
+});
